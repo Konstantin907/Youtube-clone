975786fa3096d50c051089d994799980b60e864e
konstantin <konstantinivanovic12345@gmail.com>
2023-12-22 13:32:24 -0800

    main page and responsivness done with a small padding changing on click function

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var menuIcon = document.querySelector(".menu-icon");
 var sidebar = document.querySelector(".sidebar");
+var container = document.querySelector(".container")
 
 menuIcon.onclick = function() {
     //znaci ovdje na klik hocemo prvo da prosirimo width sidebar-a, znaci kad god kliknemo nja sidebar
 //on dodaje klasu small-sidebar, a ako je dodao on ce je skloniti tako toggle radi
     sidebar.classList.toggle("small-sidebar")
+    container.classList.toggle("large-container")
 }
